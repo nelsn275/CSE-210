d601f5b8091c046752891564d2e336e1ab0a4466
Nicholas Nelson <nel19007@byui.edu>
2023-06-08 21:54:48 -0600

    Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is the Grade %: ");
+        string grade = Console.ReadLine();
+        int gradeInt = int.Parse(grade);
+        string letterGrade = "";
+        bool pass = false;
+
+
+        if (gradeInt >= 90)
+        {
+            letterGrade = "n A";
+            pass = true;
+        }
+        else if (gradeInt >= 80)
+        {
+            letterGrade = " B";
+            pass = true;
+        }
+        else if (gradeInt >= 70)
+        {
+            letterGrade = " C";
+            pass = true;
+        }
+        else if (gradeInt >= 60)
+        {
+            letterGrade = " D";
+        }
+        else
+        {
+            letterGrade = " F";
+        }
+
+        Console.WriteLine($"Your grad is a{letterGrade}");
+
+        if (pass)
+        {
+            Console.WriteLine("Congrats! You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass the class");
+        }
+
+
     }
 }
