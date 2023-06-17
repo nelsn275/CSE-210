62277861abfbf18180e55c98b320ce05e92660e1
Nicholas Nelson <nel19007@byui.edu>
2023-06-17 16:18:02 -0600

    final edits

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -11,14 +11,16 @@
             "What was the best part of my day?",
             "How did I see the hand of the Lord in my life today?",
             "What was the strongest emotion I felt today?",
-            "If I had one thing I could do over today, what would it be?"
+            "If I had one thing I could do over today, what would it be?",
+            "Was there something today that made me laugh?",
+            "How was I kind to others today?"
         };
     }
     public string GetRandomPrompt()
     {
         string randomPrompt = "";
         int randomIndex = new Random().Next(prompts.Count);
-        
+
         randomPrompt = prompts[randomIndex];
 
         return randomPrompt;
